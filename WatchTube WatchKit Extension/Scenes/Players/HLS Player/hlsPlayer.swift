b7c3b287167a1f7b5811285e485d834e8c488a43
llsc12 <42747613+llsc12@users.noreply.github.com>
2022-03-21 23:12:02 +0000

    Update hlsPlayer.swift

--- WatchTube WatchKit Extension/Scenes/Players/HLS Player/hlsPlayer.swift
+++ WatchTube WatchKit Extension/Scenes/Players/HLS Player/hlsPlayer.swift
@@ -16,8 +16,4 @@ class hlsPlayer: WKHostingController<playerView> {
     override func awake(withContext context: Any?) {
         history.addToHistory(id: UserDefaults.standard.string(forKey: hls.videoId)!)
     }
-    
-    override func willDisappear() {
-        pop()
-    }
 }
